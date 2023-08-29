e8879fa0fa230b0a77524ea45417b4dc6df06aa9
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-08-29 14:11:48 -0600

    normalized the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 
-
-
+# write your own comment -what does the next line do? normalize the json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do? output it as a table 
+streamlit.dataframe(fruityvice_normalized)
 
 
