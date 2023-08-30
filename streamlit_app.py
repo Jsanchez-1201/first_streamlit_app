7481bdc3f6d8a4969db5e565f2eaddf4d97e1689
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-08-30 09:51:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.write('The user entered ', fruit_choice)
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
+
 
 
 # write your own comment -what does the next line do? normalize the json
