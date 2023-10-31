1f5677e1bf07e1daa2a369892cbcc72ffc9312f4
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-10-31 10:34:33 -0600

    Update hello.py
    updated with my code functionality

--- hello.py
+++ hello.py
@@ -4,9 +4,10 @@ import fuzzywuzzy.process as fuzz
 import re
 import yaml
 
-# Función para mapear columnas
-def map_columns(df, reference_columns):
-    matched_columns = {}
+# Función para encontrar y emparejar columnas
+def match_columns(df, reference_columns):
+    matched_columns = {}  # Inicializar el diccionario de columnas emparejadas
+
     input_columns = df.columns.tolist()
 
     for column in input_columns:
@@ -17,8 +18,8 @@ def map_columns(df, reference_columns):
     return matched_columns
 
 # Configuración de la aplicación
-st.title('Aplicación de Mapeo de Columnas')
-st.write('Carga un archivo Excel y mapea las columnas con las de referencia.')
+st.title('Aplicación de Mapeo de Columnas y Limpieza de Datos')
+st.write('Carga un archivo Excel y mapea las columnas con las de referencia. Luego, permite al usuario realizar modificaciones en las columnas.')
 
 uploaded_file = st.file_uploader("Subir archivo Excel", type=["xlsx"])
 
@@ -44,10 +45,43 @@ if uploaded_file is not None:
                     reference_columns = []
 
             if reference_columns:
-                matched_columns = map_columns(df, reference_columns)
+                matched_columns = match_columns(df, reference_columns)
 
                 st.subheader('Columnas mapeadas:')
                 for column, mapping in matched_columns.items():
                     st.write(f"{column} -> {mapping}")
 
-                # Aquí puedes realizar otras operaciones con los datos mapeados si es necesario
+                # Eliminar las columnas que no están en reference_columns
+                columns_to_remove = [col for col in df.columns if col not in reference_columns]
+                df.drop(columns=columns_to_remove, inplace=True)
+
+                # Verificar si "Last Name" no existe y crearlo
+                if "Last Name" not in df.columns:
+                    df["Last Name"] = ""
+
+                # Permitir al usuario especificar columnas para modificación
+                st.subheader('Modificación de Columnas')
+                change_columns_input = st.text_input("Ingrese una lista de columnas para modificar (e.g., '0, 5, 7') o 'none' para omitir:")
+
+                if change_columns_input.lower() != 'none':
+                    change_columns_list = [int(col.strip()) for col in change_columns_input.split(',')]
+                    for column_index in change_columns_list:
+                        if 0 <= column_index and column_index < len(matched_columns):
+                            selected_column = list(matched_columns.keys())[column_index]
+                            st.write(f"Opciones de mapeo para la columna {column_index}: '{selected_column}':")
+                            for j, (match, score) in enumerate(matched_columns[selected_column]):
+                                st.write(f"  {j}. Mapear a '{match}' (Puntuación: {score})")  # Mostrar la coincidencia completa
+                            match_choice = st.text_input("Ingrese el número del mapeo o 'skip' para mantener como está:")
+                            if match_choice.lower() != 'skip' and match_choice.isdigit():
+                                match_index = int(match_choice)
+                                if 0 <= match_index < len(matched_columns[selected_column]):
+                                    chosen_mapping = matched_columns[selected_column][match_index][0]
+                                    df.rename(columns={selected_column: chosen_mapping}, inplace=True)
+                                    st.write(f"La columna {column_index}: '{selected_column}' se ha mapeado a '{chosen_mapping}'.")
+                            else:
+                                st.write("No se ha realizado ningún cambio en la columna.")
+                        else:
+                            st.write("Entrada no válida, elija un número o una lista de números que corresponda a una columna")
+
+                st.subheader('DataFrame Actualizado:')
+                st.write(df)
