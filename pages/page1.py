c5553811e17221dd8ad320acb69a433f18a6650c
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-02 13:59:11 -0600

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,37 +1,38 @@
 import streamlit as st
 import pandas as pd
-import yaml
 import fuzzywuzzy.process as fuzz
+import re
+import yaml
 
-# Function to find and match columns
-def match_columns(df, reference_columns):
-    matched_columns = {}  # Initialize the matched_columns dictionary
-
-    input_columns = df.columns.tolist()
-
-    for column in input_columns:
-        matches = fuzz.extractBests(column, reference_columns)
-        if matches:
-            matched_columns[column] = matches
-
-    return matched_columns
-
-# Page 1: Automated and Manual Column Mapping
 def page_1():
     st.title("Page 1: Automated and Manual Column Mapping")
 
     # Access data from st.session_state
     df = st.session_state.df
     reference_columns = st.session_state.reference_columns
-    
-    if reference_columns:
-        matched_columns = match_columns(df, reference_columns)
+
+    if df is not None:
+        last_name_pattern = re.compile(r'\b[lL][aA][sS][tT]\s?[nN][aA][mM][eE]\b|\bLST\s?NM\b')
+
+        # Function to find and match columns
+        def match_columns(df, reference_columns):
+            matched_columns = {}  # Initialize the matched_columns dictionary
+            input_columns = df.columns.tolist()
+
+            for column in input_columns:
+                matches = fuzz.extractBests(column, reference_columns)
+                if matches:
+                    matched_columns[column] = matches
+
+            return matched_columns
+
+        if reference_columns:
+            matched_columns = match_columns(df, reference_columns)
 
         st.subheader('Mapped Columns:')
         mapped_columns_text = ""
         for column_index, (column, mapping) in enumerate(matched_columns.items()):
             mapped_columns_text += f"{column_index}. '{column}' is initially mapped to '{mapping[0][0]}'\n"
-
         st.text(mapped_columns_text)
 
         # Allow the user to specify columns for modification
@@ -55,10 +56,12 @@ def page_1():
                             st.write(f"Column {column_index}: '{selected_column}' has been mapped to '{chosen_mapping}'.")
                     else:
                         st.write("No changes have been made to the column.")
+
         # Check if "Last Name" doesn't exist and create it
         if "Last Name" not in df.columns:
             df["Last Name"] = ""
-    
-    st.subheader('Updated DataFrame:')
-    st.write(df)
 
+        st.subheader('Updated DataFrame:')
+        st.write(df)
+
+page_1()
