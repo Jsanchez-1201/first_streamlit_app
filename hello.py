cf86c7fce2df45fc308bffe4bacb081e242a63d8
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-02 13:18:39 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from pages import page1
 
 st.set_page_config(page_title="Multi-Page App")
 
-st.title("Welcome to the Multi-Page Streamlit App")
+st.title("Welcome to the Marketing Data Cleaning App")
 
 # Unique keys for file upload widgets
 uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx"], key="excel_uploader")
@@ -32,5 +32,4 @@ if not st.session_state.page_1_started:
         st.session_state.page_1_started = True
         st.session_state.df = df
         st.session_state.reference_columns = reference_columns
-        st.title("Page 1: Automated and Manual Column Mapping")
         page1.page_1()
