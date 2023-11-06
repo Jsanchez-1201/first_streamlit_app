ac45ff7247841e581fbe3cb303fb18b69a27acf2
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-06 13:34:54 -0600

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -26,40 +26,34 @@ def page_1():
         return matched_columns
 
     if df is not None:
-        matched_columns = match_columns(df, reference_columns)
+        if "mapped_columns" not in st.session_state:
+            # Perform initial automated mapping only once
+            matched_columns = match_columns(df, reference_columns)
+            st.session_state.mapped_columns = matched_columns
 
+        # Display the mapped columns
         st.subheader('Mapped Columns:')
         mapped_columns_text = ""
-        for column_index, (column, mapping) in enumerate(matched_columns.items()):
+        for column_index, (column, mapping) in enumerate(st.session_state.mapped_columns.items()):
             mapped_columns_text += f"{column_index}. '{column}' is initially mapped to '{mapping[0][0]}'\n"
         st.text(mapped_columns_text)
 
-        # Initial automated mapping
-        for column_index, (column, mapping) in enumerate(matched_columns.items()):
-            df.rename(columns={column: mapping[0][0]}, inplace=True)
-
-        # Check if "Last Name" doesn't exist and create it
-        if "Last Name" not in df.columns:
-            df["Last Name"] = ""
-
-        # Allow the user to specify columns for modification
-        st.subheader('Column Modification')
+        # Perform manual mapping only if a list of columns is provided
         change_columns_input = st.text_input("Enter a list of columns to modify (e.g., '0, 1, 2') or 'none' to skip:")
-
         if change_columns_input.lower() != 'none':
             change_columns_list = [int(col.strip()) for col in change_columns_input.split(',') if col.strip()]
-            modified = False  # To track if any column is modified
+            modified = False
             for column_index in change_columns_list:
-                if 0 <= column_index < len(matched_columns):
-                    selected_column = list(matched_columns.keys())[column_index]
+                if 0 <= column_index < len(st.session_state.mapped_columns):
+                    selected_column = list(st.session_state.mapped_columns.keys())[column_index]
                     st.write(f"Mapping options for column {column_index}: '{selected_column}':")
-                    for j, (match, score) in enumerate(matched_columns[selected_column]):
+                    for j, (match, score) in enumerate(st.session_state.mapped_columns[selected_column]):
                         st.write(f"  {j}. Map to '{match}' (Score: {score})")  # Display all mapping options
                     match_choice = st.text_input("Enter the number for the mapping, or 'skip' to keep as is:")
                     if match_choice.lower() != 'skip' and match_choice.isdigit():
                         match_index = int(match_choice)
-                        if 0 <= match_index < len(matched_columns[selected_column]):
-                            chosen_mapping = matched_columns[selected_column][match_index][0]
+                        if 0 <= match_index < len(st.session_state.mapped_columns[selected_column]):
+                            chosen_mapping = st.session_state.mapped_columns[selected_column][match_index][0]
                             df.rename(columns={selected_column: chosen_mapping}, inplace=True)
                             modified = True
                             st.write(f"Column {column_index}: '{selected_column}' has been mapped to '{chosen_mapping}'.")
@@ -70,6 +64,11 @@ def page_1():
         columns_to_remove = [col for col in df.columns if col not in reference_columns]
         df.drop(columns=columns_to_remove, inplace=True)
 
+        # Add the "Last Name" column if it doesn't exist
+        if "Last Name" not in df.columns:
+            df["Last Name"] = ""
+
+        # Display the updated DataFrame
         st.subheader('Updated DataFrame:')
         st.write(df)
 
