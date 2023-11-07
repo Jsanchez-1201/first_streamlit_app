b6d2d17ebf574ecc33e70ba19ff0f74b46f1fe56
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-07 09:36:38 -0600

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -38,28 +38,28 @@ def page_1():
             mapped_columns_text += f"{column_index}. '{column}' is initially mapped to '{mapping[0][0]}'\n"
         st.text(mapped_columns_text)
 
-       st.subheader('Modificación de Columnas')
-                change_columns_input = st.text_input("Enter a list of columns to modify (e.g., '0, 1, 2') or 'none' to skip:")
+        st.subheader('Column Modification')
+        change_columns_input = st.text_input("Enter a list of columns to modify (e.g., '0, 1, 2') or 'none' to skip:")
 
-                if change_columns_input.lower() != 'none':
-                    change_columns_list = [int(col.strip()) for col in change_columns_input.split(',') if col.strip()]
-                    for column_index in change_columns_list:
-                        if 0 <= column_index and column_index < len(matched_columns):
-                            selected_column = list(matched_columns.keys())[column_index]
-                            st.write(f"Mapping options for column {column_index}: '{selected_column}':")
-                            for j, (match, score) in enumerate(matched_columns[selected_column]):
-                                st.write(f"  {j}. Map to '{match}' (Score: {score})") 
-                            match_choice = st.text_input("Enter the number for the mapping, or 'skip' to keep as is:")
-                            if match_choice.lower() != 'skip' and match_choice.isdigit():
-                                match_index = int(match_choice)
-                                if 0 <= match_index < len(matched_columns[selected_column]):
-                                    chosen_mapping = matched_columns[selected_column][match_index][0]
-                                    df.rename(columns={selected_column: chosen_mapping}, inplace=True)
-                                    st.write(f"Column {column_index}: '{selected_column}' has been mapped to '{chosen_mapping}'.")
-                            else:
-                                st.write("No changes have been made to the columns.")
-                        else:
-                            st.write("Invalid input, choose a number or list of numbers corresponding to a column or columns")
+        if change_columns_input.lower() != 'none':
+            change_columns_list = [int(col.strip()) for col in change_columns_input.split(',') if col.strip()]
+            for column_index in change_columns_list:
+                if 0 <= column_index and column_index < len(matched_columns):
+                    selected_column = list(matched_columns.keys())[column_index]
+                    st.write(f"Mapping options for column {column_index}: '{selected_column}':")
+                    for j, (match, score) in enumerate(matched_columns[selected_column]):
+                        st.write(f"  {j}. Map to '{match}' (Score: {score})")
+                    match_choice = st.text_input("Enter the number for the mapping, or 'skip' to keep as is:")
+                    if match_choice.lower() != 'skip' and match_choice.isdigit():
+                        match_index = int(match_choice)
+                        if 0 <= match_index < len(matched_columns[selected_column]):
+                            chosen_mapping = matched_columns[selected_column][match_index][0]
+                            df.rename(columns={selected_column: chosen_mapping}, inplace=True)
+                            st.write(f"Column {column_index}: '{selected_column}' has been mapped to '{chosen_mapping}'.")
+                    else:
+                        st.write("No changes have been made to the columns.")
+                else:
+                    st.write("Invalid input, choose a number or list of numbers corresponding to a column or columns")
 
         # Remove columns that are not in reference_columns in the updated DataFrame
         columns_to_remove = [col for col in df.columns if col not in reference_columns]
