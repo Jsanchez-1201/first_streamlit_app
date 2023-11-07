e9a3978e129c4d579b65186a65fbf91385ec9ecd
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-07 09:32:33 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import fuzzywuzzy.process as fuzz
-import re
 import yaml
+from pages import page1
 
 st.set_page_config(page_title="Multi-Page App")
 
@@ -16,6 +15,10 @@ reference_file = st.file_uploader("Upload YAML file with reference columns (opti
 df = None
 reference_columns = []
 
+# Pre-load the standard_columns.yml
+with open('standard_columns.yml', 'r') as default_yaml:
+    default_reference_columns = yaml.safe_load(default_yaml)
+
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     if reference_file is not None:
@@ -25,20 +28,16 @@ if uploaded_file is not None:
             except Exception as e:
                 st.error(f"Error loading reference columns: {str(e)}")
 
+# Pass data to Page 1 using st.session_state
+st.session_state.df = df
+st.session_state.reference_columns = reference_columns
+
+# Redirect to Page 1
+if st.session_state.df is not None and st.session_state.reference_columns:
+    page1.page_1()
+
+#Add a button to continue with the default yaml
 if st.button("Continue with Default YAML"):
     st.session_state.df = df
-    st.session_state.reference_columns = reference_columns
-    st.session_state.default_yaml = True
-
-if not hasattr(st.session_state, 'df'):
-    st.session_state.df = None
-if not hasattr(st.session_state, 'reference_columns'):
-    st.session_state.reference_columns = None
-if not hasattr(st.session_state, 'default_yaml'):
-    st.session_state.default_yaml = False
-
-if st.session_state.df is not None:
-    if st.session_state.default_yaml:
-        st.write("Using the default YAML.")
-    else:
-        st.write("Using the uploaded YAML.")
+    st.session_state.reference_columns = default_reference_columns  # Assign the pre-loaded default YAML
+    page1.page_1()
