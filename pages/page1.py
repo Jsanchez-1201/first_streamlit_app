dc42eedd8c5a8ba5189c41ccb09f1130bc7c5045
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-07 11:51:09 -0600

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import fuzzywuzzy.process as fuzz
 import re
-import yaml
 
 def page_1():
     st.title("Page 1: Automated and Manual Column Mapping")
@@ -38,33 +37,34 @@ def page_1():
             mapped_columns_text += f"{column_index}. '{column}' is initially mapped to '{mapping[0][0]}'\n"
         st.text(mapped_columns_text)
 
+        # Add st.form to the user input section
         st.subheader('Column Modification')
-        change_columns_input = st.text_input("Enter a list of columns to modify (e.g., '0, 5, 7') or 'none' to skip:")
+        with st.form(key='user_input_form'):
+            change_columns_input = st.text_input("Enter a list of columns to modify (e.g., '0, 5, 7') or 'none' to skip:")
+            submit_button = st.form_submit_button(label='Submit')
 
-        if change_columns_input.lower() != 'none':
-            change_columns_list = [int(col.strip()) for col in change_columns_input.split(',') if col.strip()]
-            for column_index in change_columns_list:
-                if 0 <= column_index and column_index < len(matched_columns):
-                    selected_column = list(matched_columns.keys())[column_index]
-                    st.write(f"Mapping options for column {column_index}: '{selected_column}':")
-                    for j, (match, score) in enumerate(matched_columns[selected_column]):
-                        st.write(f"  {j}. Map to '{match}' (Score: {score})")  # Display the full match
-                    match_choice = st.text_input("Enter the number for the mapping, or 'skip' to keep as is:")
-                    if match_choice.lower() != 'skip' and match_choice.isdigit():
-                        match_index = int(match_choice)
-                        if 0 <= match_index < len(matched_columns[selected_column]):
-                            chosen_mapping = matched_columns[selected_column][match_index][0]
-                            df.rename(columns={selected_column: chosen_mapping}, inplace=True)
-                            st.write(f"Column {column_index}: '{selected_column}' has been mapped to '{chosen_mapping}'.")
+            if submit_button:
+                if change_columns_input.lower() != 'none':
+                    change_columns_list = [int(col.strip()) for col in change_columns_input.split(',') if col.strip()]
+                    for column_index in change_columns_list:
+                        if 0 <= column_index and column_index < len(matched_columns):
+                            selected_column = list(matched_columns.keys())[column_index]
+                            st.write(f"Mapping options for column {column_index}: '{selected_column}':")
+                            for j, (match, score) in enumerate(matched_columns[selected_column]):
+                                st.write(f"  {j}. Map to '{match}' (Score: {score})")  # Display the full match
+                            match_choice = st.text_input("Enter the number for the mapping, or 'skip' to keep as is:")
+                            if match_choice.lower() != 'skip' and match_choice.isdigit():
+                                match_index = int(match_choice)
+                                if 0 <= match_index < len(matched_columns[selected_column]):
+                                    chosen_mapping = matched_columns[selected_column][match_index][0]
+                                    df.rename(columns={selected_column: chosen_mapping}, inplace=True)
+                                    st.write(f"Column {column_index}: '{selected_column}' has been mapped to '{chosen_mapping}'.")
+                                else:
+                                    st.write("No changes have been made to the columns.")
+                            else:
+                                st.write("Invalid input. Please enter a valid number or 'skip'.")
                         else:
-                            st.write("No changes have been made to the columns.")
-                    else:
-                        st.write("Invalid input. Please enter a valid number or 'skip'.")
-                else:
-                    st.write("Invalid input, please choose a number or a list of numbers corresponding to a column")
-
-
-
+                            st.write("Invalid input, please choose a number or a list of numbers corresponding to a column")
 
         # Remove columns that are not in reference_columns in the updated DataFrame
         columns_to_remove = [col for col in df.columns if col not in reference_columns]
@@ -77,3 +77,6 @@ def page_1():
         # Display the updated DataFrame
         st.subheader('Updated DataFrame:')
         st.write(df)
+
+if __name__ == "__main__":
+    page_1()
