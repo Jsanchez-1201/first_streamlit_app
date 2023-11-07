207bd7ab432e71301cd3ce8afe9dab563e313f43
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-07 09:28:37 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import yaml
-from pages import page1
+import page1
 
 st.set_page_config(page_title="Multi-Page App")
 
@@ -24,20 +24,10 @@ if uploaded_file is not None:
             except Exception as e:
                 st.error(f"Error loading reference columns: {str(e)}")
 
-# Initialize a flag to control page navigation
-show_page1 = False
-
 # Add a button to proceed with default YAML
 if st.button("Continue with Default YAML"):
-    show_page1 = True
-
-if show_page1:
-    # Pass data to Page 1 using st.session_state
     st.session_state.df = df
     st.session_state.reference_columns = reference_columns
     page1.page_1()
-
-
-
 
 
