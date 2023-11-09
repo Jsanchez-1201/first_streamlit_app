a440351b10be9a8cc5f7d269b894f1b5c12df450
Jsanchez-1201 <143029087+Jsanchez-1201@users.noreply.github.com>
2023-11-09 16:52:01 -0600

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -82,48 +82,48 @@ def page_2():
         return clusters, old_names, header_name
     
     def editing_cluster(data):
-            # calling the clustering function
-            editing_clusters, old_names, header_name = clustering(data) # run
-            while True:
-                st.write("Current Clusters:")
-                for cluster_label, cluster_items in editing_clusters.items():
-                    st.write(f"Cluster {cluster_label}: {cluster_items}")
-                choice = st.selectbox("Options for editing clusters:",("Move an item to another cluster","Merge clusters", "Split a cluster","Create a new cluster","Edit the information within the cluster", "Skip"))
-                if choice == "Move an item to another cluster":
-                    item_to_move = st.text_input("Enter the item you want to move:", key="item_to_move_input")
-                    from_cluster_id = st.text_input("Enter the current cluster ID:", key="from_cluster_id_input")
-                    to_cluster_id = st.text_input("Enter the target cluster ID:", key="to_cluster_id_input")
-                # Add a button to trigger the distance matrix computation
-                    submit_button = st.button("Move item")
-                    if submit_button:
-                        st.session_state.item_to_move = item_to_move  # Update session state
-                        st.session_state.from_cluster_id = from_cluster_id
-                        if item_to_move in editing_clusters[from_cluster_id]:
-                            editing_clusters[from_cluster_id].remove(item_to_move)
-                            editing_clusters[to_cluster_id].append(item_to_move)
-                        st.session_state.editing_clusters = editing_clusters
-                elif choice == "Merge clusters":
-                    cluster1_id = st.text_input("Enter the first cluster ID to merge: ")
-                    cluster2_id = st.text_input("Enter the second cluster ID to merge: ")
-                    submit_button = st.button("Merge item")
-                    if submit_button:
-                        st.session_state.cluster1_id = cluster1_id  # Update session state
-                        st.session_state.cluster2_id = cluster2_id
-                        editing_clusters[cluster1_id] += editing_clusters[cluster2_id]
-                        del editing_clusters[cluster2_id]
-                        st.session_state.editing_clusters = editing_clusters
-                elif choice == "Split a cluster":
-                    cluster_id = st.text_input("Enter the cluster ID to split: ")
-                    item_to_split = st.text_input("Enter the item you want to split: ")
-                    submit_button = st.button("Split a cluster")
-                    if submit_button:
-                        st.session_state.cluster_id = cluster_id  # Update session state
-                        st.session_state.item_to_split = item_to_split
-                        if item_to_split in editing_clusters[cluster_id]:
-                            editing_clusters[cluster_id].remove(item_to_split)
-                            new_cluster_id = max(editing_clusters.keys()) + 1 # could be an error
-                            editing_clusters[new_cluster_id] = [item_to_split]
-                        st.session_state.editing_clusters = editing_clusters
+        # calling the clustering function
+        editing_clusters, old_names, header_name = clustering(data) # run
+        while True:
+            st.write("Current Clusters:")
+            for cluster_label, cluster_items in editing_clusters.items():
+                st.write(f"Cluster {cluster_label}: {cluster_items}")
+            choice = st.selectbox("Options for editing clusters:",("Move an item to another cluster","Merge clusters", "Split a cluster","Create a new cluster","Edit the information within the cluster", "Skip"))
+            if choice == "Move an item to another cluster":
+                item_to_move = st.text_input("Enter the item you want to move:", key="item_to_move_input")
+                from_cluster_id = st.text_input("Enter the current cluster ID:", key="from_cluster_id_input")
+                to_cluster_id = st.text_input("Enter the target cluster ID:", key="to_cluster_id_input")
+            # Add a button to trigger the distance matrix computation
+                submit_button = st.button("Move item")
+                if submit_button:
+                    st.session_state.item_to_move = item_to_move  # Update session state
+                    st.session_state.from_cluster_id = from_cluster_id
+                    if item_to_move in editing_clusters[from_cluster_id]:
+                        editing_clusters[from_cluster_id].remove(item_to_move)
+                        editing_clusters[to_cluster_id].append(item_to_move)
+                    st.session_state.editing_clusters = editing_clusters
+            elif choice == "Merge clusters":
+                cluster1_id = st.text_input("Enter the first cluster ID to merge: ")
+                cluster2_id = st.text_input("Enter the second cluster ID to merge: ")
+                submit_button = st.button("Merge item")
+                if submit_button:
+                    st.session_state.cluster1_id = cluster1_id  # Update session state
+                    st.session_state.cluster2_id = cluster2_id
+                    editing_clusters[cluster1_id] += editing_clusters[cluster2_id]
+                    del editing_clusters[cluster2_id]
+                    st.session_state.editing_clusters = editing_clusters
+            elif choice == "Split a cluster":
+                cluster_id = st.text_input("Enter the cluster ID to split: ")
+                item_to_split = st.text_input("Enter the item you want to split: ")
+                submit_button = st.button("Split a cluster")
+                if submit_button:
+                    st.session_state.cluster_id = cluster_id  # Update session state
+                    st.session_state.item_to_split = item_to_split
+                    if item_to_split in editing_clusters[cluster_id]:
+                        editing_clusters[cluster_id].remove(item_to_split)
+                        new_cluster_id = max(editing_clusters.keys()) + 1 # could be an error
+                        editing_clusters[new_cluster_id] = [item_to_split]
+                    st.session_state.editing_clusters = editing_clusters
         editing_clusters = editing_cluster(updated_dataframe)
         return editing_clusters
         #         elif choice == "4":
